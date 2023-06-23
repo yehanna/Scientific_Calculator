b3e4b99e1c2b5ba6e34a2e397f48d8b2ecd18009
Hanamariam Yehuala <101621682+yehanna@users.noreply.github.com>
2023-06-23 11:42:59 +0300

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/hanan/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/hanan/Desktop/s.cal1/s_c" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/hanan/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/hanan/Desktop/s.cal1/s_c"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGQ0YjQwYzY1OTYyY2VhMDJlNWQ3ODE4MGU5MTQ0Njk2ODQzNmU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/hanan/Desktop/s.cal1/s_c/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/hanan/Desktop/s.cal1/s_c/lib/main.dart"
+)
