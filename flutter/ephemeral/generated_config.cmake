1907b17f7090b4f78d39fcdfc9e1ca49bd4fc920
Hanamariam Yehuala <101621682+yehanna@users.noreply.github.com>
2023-06-24 19:32:57 +0300

    Add files via upload

---
+++ flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Hann\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\search computer\\Desktop\\my app\\Group13-scientific calculator" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Hann\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\search computer\\Desktop\\my app\\Group13-scientific calculator"
+  "FLUTTER_ROOT=C:\\Hann\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\search computer\\Desktop\\my app\\Group13-scientific calculator\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\search computer\\Desktop\\my app\\Group13-scientific calculator"
+  "FLUTTER_TARGET=C:\\Users\\search computer\\Desktop\\my app\\Group13-scientific calculator\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80NWY2ZTAwOTExMGRmNGYzNGVjMmNmOTlmNjNjZjczYjcxYjdhNDIwLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\search computer\\Desktop\\my app\\Group13-scientific calculator\\.dart_tool\\package_config.json"
+)
